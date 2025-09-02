e76beb15dd216efa378671d2fa5c6ba143802bfb
uzairamin01 <uzairamin476@gmail.com>
2025-09-03 00:11:12 +0500

    Resolve Errors

--- js/about.js
+++ js/about.js
@@ -2,26 +2,7 @@ fetch("/components/nav.html")
     .then(response => response.text())
     .then(data => {
         document.getElementById("nav").innerHTML = data;
-
-        // Set active class based on current page
-        const navItems = document.querySelectorAll('#nav ul li');
-        const pages = ['/index.html', './pages/about.html', './pages/services.html', './pages/contact.html'];
-        let currentPage = window.location.pathname.split('/').pop();
-        if (currentPage === '' || currentPage === '/index.html') {
-            currentPage = '/index.html';
-        }
-
-        navItems.forEach((item, idx) => {
-            if (currentPage === pages[idx].split('/').pop()) {
-                item.classList.add('active');
-            }
-            item.addEventListener('click', () => {
-                navItems.forEach(li => li.classList.remove('active'));
-                item.classList.add('active');
-                window.location.href = pages[idx]
-            });
-        });
-    });
+    })
 
 window.addEventListener('scroll', function () {
     const nav = document.querySelector('nav');
